8e8d8f9506e29181c440673e5b212b2dca1b9b73
Michał Radecki <michal.radecki.dev@gmail.com>
2026-08-29 17:37:52 +0200

    Initial commit with .gitignore generated with start.spring.io

---
+++ src/test/java/michal/radecki/request_management/RequestManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package michal.radecki.request_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
