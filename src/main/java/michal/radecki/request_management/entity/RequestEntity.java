8695585f5bf52f7f9a47e83f9f2f9eb602937e9b
Michał Radecki <michal.radecki.dev@gmail.com>
2026-08-30 16:18:14 +0200

    Add missing @Enumerated annotation to RequestEntity state

--- src/main/java/michal/radecki/request_management/entity/RequestEntity.java
+++ src/main/java/michal/radecki/request_management/entity/RequestEntity.java
@@ -2,6 +2,8 @@ package michal.radecki.request_management.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -24,6 +26,7 @@ public class RequestEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer requestId;
+    @Enumerated(EnumType.STRING)
     private RequestState state;
     private String name;
     private String body;
